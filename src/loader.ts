be8c461679b91e87108c36685f5009a6ac7d3277
Rahul Ghosal <rdghosal@gmail.com>
2023-05-07 08:58:00 -0400

    refactor: move static files into static and use path module to load csv in loader

---
+++ src/loader.ts
@@ -0,0 +1,22 @@
+import * as fs from "fs";
+import * as Papa from "papaparse";
+import * as path from "path";
+
+/*
+ * TODO
+ * 1. load csv data into sqlite, with validation.
+ * 2. read data from db.
+ * 3. make big ol' object where lines of credit are the parent to L/Cs and Import Bills
+ *   Assume all Import Bills are usuance.
+ * 4. print L/Cs and Import Bills that are orphaned. (save to a .log?)
+ * 5. spit out the json for line of credits and their children that check out
+ */
+const s: fs.ReadStream = fs.createReadStream(path.join(path.dirname(__dirname), 'static', 'test.csv'));
+Papa.parse(s, {
+    header: true,
+    complete: (results) => {
+        console.log(results.data);
+    }
+});
+
+
